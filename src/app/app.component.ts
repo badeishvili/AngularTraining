aaaea87b730ef6de327bca86facb1deafd81c06c
badeishvili <badeishvili@gmail.com>
2025-03-26 19:04:18 +0400

    l

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HelloComp } from "./comp1/comp1.component";
+import {WSFirstFormComponent} from './wsfirst-form/wsfirst-form.component';
+import {AppDirective} from './mydirective.directive';
+import {TruncatePipe} from './textcutpipe.pipe';
+import {CustomTransformPipe} from './registerpipe.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HelloComp, WSFirstFormComponent, AppDirective, TruncatePipe, CustomTransformPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
